b121c8d1253686fa0650c6b88237a0fec0600d07
Gordan Trevis <GitHub@gordantrevis.me>
2018-11-27 17:17:11 +0800

    css Class fix

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -53,7 +53,7 @@ $.getJSON( fanUrl2, function( json ) {
   console.log( "JSON Data: " + json.data.aqi );
   var air = json.data.aqi
   text.text(air)
-  odometer.innerHTML = air
+  odometer_liangxiang.innerHTML = air
   return json
  });
 
